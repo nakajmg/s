0d221d89237a2ad06b7b160a5d29c1bd95ced932
nakajmg <nakajima.jmg@gmail.com>
2016-11-08 22:14:02 +0900

    add template

---
+++ vue/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  pages: [
+    {
+      attributes: {
+        layout: 'cover',
+        invert: '',
+        backface: './img/bg.png',
+        backfaceFilter: 'blur(2px) brightness(1.5)'
+      },
+      note: ''
+    },
+    {
+      attributes: {
+        layout: 'bullets'
+      }
+    }
+  ]
+}
---
+++ vue/index.md
@@ -0,0 +1,13 @@
+# Vue.jsとか
+
+2016/11/19 - 静岡JavaScript勉強会 #4 [@nakajmg](https://twitter.com/nakajmg)
+
+#shizuoka_js
+
+-----
+
+## じまぐです
+
+- 株式会社ピクセルグリッド
+- フロントエンド・エンジニア
+- [Aigis](https://pxgrid.github.io/aigis/)
